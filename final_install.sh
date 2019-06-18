b8c177ce3fba68401b91412000c49fb366734eeb
ianwevans-wwt <36733862+ianwevans-wwt@users.noreply.github.com>
2019-06-17 23:56:38 -0400

    Add files via upload

---
+++ final_install.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# init
+# Created 6-16-19 by evansi @ WWT
+ 
+ln -s /proc/self/mounts /etc/mtab
+apt update && install -y nano
+LANG=en_US.UTF-8 locale-gen --purge en_US.UTF-8
+apt install --yes --no-install-recommends linux-image-generic
+apt install --yes zfs-initramfs
+apt install dosfstools
+mkdosfs -F 32 -s 1 -n EFI /dev/sda2
+mkdir /boot/efi
+echo PARTUUID=$(blkid -s PARTUUID -o value \
+     /dev/sda2) \
+     /boot/efi vfat nofail,x-systemd.device-timeout=1 0 1 >> /etc/fstab
+mount /boot/efi
+apt install --yes grub-efi-amd64-signed shim-signed
+apt install --yes nano
+apt install --yes edubuntu-desktop
+apt install --yes xubuntu-desktop
+apt install --yes gdebi-core wget
+apt install --yes msttcorefonts
+apt install --yes gcompris
+apt install --yes chromium-browser
+apt install --yes hunspell-es
+apt install --yes hypehn-es
+apt install --yes aspell-es
+apt install --yes language-pack-es
+apt install --yes qalculate-gtk
+apt install --yes calibre
+apt install --yes pdfmod
+apt install --yes scribus
+apt install --yes codeblocks
+apt install --yes ninja-ide
+apt install --yes scratch
+apt install --yes kdeedu
+apt install --yes tuxpaint
+apt install --yes tuxtype
+apt install --yes blender
+apt install --yes gimp
+apt install --yes lmms
+apt install --yes synaptic
+apt install --yes qcalculate
+apt install --yes libreoffice-l10n-es
+apt install --yes mypaint
+apt install --yes default-jre
+apt install --yes default-jdk
+apt install --yes arduino-core
+apt install --yes freecad
+apt install --yes desktop-file-utils
+apt-install --yes snap
+snap install code --classic
+snap install vlc
+apt install --yes browser-plugin-vlc
+apt install --yes exfat-utils
+apt install --yes fonts-lyx
+apt install --yes xfce4
+apt install --yes dconf-tools
+apt install --yes libglib2.0-bin
+apt install --yes gdm3
+apt-install --yes gnome-common
+apt-install --yes xscreensaver xscreensaver-gl-extra xscreensaver-data-extra
+wget http://archive.ubuntu.com/ubuntu/pool/universe/a/arduino/arduino_1.0.5+dfsg2-4.1_all.deb
+dpkg -i arduino_1.0.5+dfsg2-4.1_all.deb
+apt-get -f install; rm arduino_1.0.5+dfsg2-4.1_all.deb
+wget -O google-earth64.deb http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
+dpkg -i google-earth64.deb
+apt-get -f install; rm google-earth64.deb
+passwd local
+passwd
+hostname prbasesystem
+cp ./zfs-import-bpool.service /etc/systemd/system/
+systemctl enable zfs-import-bpool.service
+cp /usr/share/systemd/tmp.mount /etc/systemd/system/
+systemctl enable tmp.mount
+grub-probe /boot
+update-initramfs -u -k all
+sed -i '/GRUB_CMDLINE_LINUX=""/s/^/#/g' /etc/default/grub
+echo "GRUB_CMDLINE_LINUX="\"root=ZFS=rpool/ROOT/ubuntu"\"\n" >> /etc/default/grub
+update-grub
+grub-install --target=x86_64-efi --efi-directory=/boot/efi \
+      --bootloader-id=ubuntu --recheck --no-floppy
+ls /boot/grub/*/zfs.mod
+umount /boot/efi
+zfs set mountpoint=legacy bpool/BOOT/ubuntu
+echo bpool/BOOT/ubuntu /boot zfs \
+      nodev,relatime,x-systemd.requires=zfs-import-bpool.service 0 0 >> /etc/fstab
+zfs set mountpoint=legacy rpool/var/log
+echo rpool/var/log /var/log zfs nodev,relatime 0 0 >> /etc/fstab
+zfs set mountpoint=legacy rpool/var/spool
+echo rpool/var/spool /var/spool zfs nodev,relatime 0 0 >> /etc/fstab
+zfs set mountpoint=legacy rpool/var/tmp
+echo rpool/var/tmp /var/tmp zfs nodev,relatime 0 0 >> /etc/fstab
+zfs snapshot bpool/BOOT/ubuntu@install
+zfs snapshot rpool/ROOT/ubuntu@install
+exit
+mount | grep -v zfs | tac | awk '/\/mnt/ {print $3}' | xargs -i{} umount -lf {}
+zpool export -a
+echo Done! Reboot the machine and run final_scripts.sh under /etc/scripts to apply final settings.
---
+++ zfs_install.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# init
+# Created 6-16-19 by evansi @ WWT
+
+apt-add-repository universe
+apt update
+dd if=/dev/zero of=/dev/sda bs=512 count=1 conv=notrunc
+apt install --yes debootstrap gdisk zfs-initramfs openssh-server
+sgdisk --zap-all /dev/sda
+apt install zfs-initramfs gdisk debootstrap
+sgdisk     -n2:1M:+512M   -t2:EF00 /dev/sda
+sgdisk     -n3:0:+512M    -t3:BF01 /dev/sda
+sgdisk     -n4:0:0        -t4:BF01 /dev/sda
+zpool create -f -o ashift=12 -d \
+      -o feature@async_destroy=enabled \
+      -o feature@bookmarks=enabled \
+      -o feature@embedded_data=enabled \
+      -o feature@empty_bpobj=enabled \
+      -o feature@enabled_txg=enabled \
+      -o feature@extensible_dataset=enabled \
+      -o feature@filesystem_limits=enabled \
+      -o feature@hole_birth=enabled \
+      -o feature@large_blocks=enabled \
+      -o feature@lz4_compress=enabled \
+      -o feature@spacemap_histogram=enabled \
+      -o feature@userobj_accounting=enabled \
+      -O acltype=posixacl -O canmount=off -O compression=lz4 -O devices=off \
+      -O normalization=formD -O relatime=on -O xattr=sa \
+      -O mountpoint=/ -R /mnt \
+      bpool /dev/sda3
+zpool create -f -o ashift=12 \
+      -O acltype=posixacl -O canmount=off -O compression=lz4 \
+      -O dnodesize=auto -O normalization=formD -O relatime=on -O xattr=sa \
+      -O mountpoint=/ -R /mnt \
+      rpool /dev/sda4
+zfs create -o canmount=off -o mountpoint=none rpool/ROOT
+zfs create -o canmount=off -o mountpoint=none bpool/BOOT
+zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/ubuntu
+zfs mount rpool/ROOT/ubuntu
+zfs create -o canmount=noauto -o mountpoint=/boot bpool/BOOT/ubuntu
+zfs mount bpool/BOOT/ubuntu
+zfs create                                 rpool/home
+zfs create -o mountpoint=/root             rpool/home/root
+zfs create -o canmount=off                 rpool/var
+zfs create -o canmount=off                 rpool/var/lib
+zfs create                                 rpool/var/log
+zfs create                                 rpool/var/spool
+zfs create -o com.sun:auto-snapshot=false  rpool/var/cache
+zfs create -o com.sun:auto-snapshot=false  rpool/var/tmp
+chmod 1777 /mnt/var/tmp
+zfs create                                 rpool/var/games
+debootstrap bionic /mnt
+zfs set devices=off rpool
+
+cat >> /mnt/etc/netplan/eno1.yaml << EOF
+
+network:
+ version: 2
+ renderer: networkd
+ ethernets:
+   eno1:
+     dhcp4: yes
+     dhcp6: no
+EOF
+
+cat <<EOF >> /mnt/etc/apt/sources.list
+deb http://archive.ubuntu.com/ubuntu bionic main universe
+deb-src http://archive.ubuntu.com/ubuntu bionic main universe
+
+deb http://security.ubuntu.com/ubuntu bionic-security main universe
+deb-src http://security.ubuntu.com/ubuntu bionic-security main universe
+
+deb http://archive.ubuntu.com/ubuntu bionic-updates main universe
+deb-src http://archive.ubuntu.com/ubuntu bionic-updates main universe
+EOF
+
+mount --rbind /dev  /mnt/dev
+mount --rbind /proc /mnt/proc
+mount --rbind /sys  /mnt/sys
+mkdir /mnt/etc/scripts
+chmod -R 777 /mnt/etc/scripts
+cp ./zfs-import-bpool.service /mnt/etc/scripts
+cp ./zfs.conf /mnt/etc/modprobe.d
+cp ./final_install.sh /mnt/etc/scripts
+cp ./final_scripts.sh /mnt/etc/scripts
+cp -r ./packages /mnt/etc/scripts
+cp ./zfs.conf /etc/modprobe.d
+chroot /mnt /bin/bash --login
+
+
+
